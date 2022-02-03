e1ee2479e237678e18157ef5203563646a7477cd
Admish-prasoon <98838056+Admish-prasoon@users.noreply.github.com>
2022-02-03 22:39:49 +0530

    Update adhneet.py

--- adhneet.py
+++ adhneet.py
@@ -12,10 +12,14 @@ passwordss = input("\033[1;95m[+]\033[1;95m Password :\033[1;95m ")
 
 if (passwordss == '12345'):
       print("Password is week login blocked")
+      print("\033[1;97m")
 else:
       print("\033[1;97m[1]see info")
+      print("\033[1;97m[1]shutdown")
+      print("\033[1;97m")
       inp = input("[?]: ")
       if (inp == '1'):
            print("\033[1;92musername:" + username)
            print("password:" + passwordss)
+            print("\033[1;97m")
          
